be6d273e2e4e87d7c251ac2b70025e02ab6f6248
robertwagar <robertwagar@gmail.com>
2021-02-09 15:04:29 +0100

    Themss and toggle

---
+++ src/components/Themes/Themes.js
@@ -0,0 +1,27 @@
+
+import light from '../../img/connected.jpg';
+import dark from '../../img/disconnected.jpg';
+
+
+export const lightTheme = {
+    body: `url(${light})`,
+    text: '#F0F4F8',
+    textShadow: 'rgba(0,0,0)',
+    background: '#1e2e3e',
+    logo: '#f0f4f8',
+    btnBg: '#603f83a1',
+    btnHover: 'rgba(0,0,0,0.8)',
+    btnColor: '#F0F4F8',
+        }
+
+export const darkTheme = {
+    body: `url(${dark})`,
+    text: '#6dd47e',
+    textShadow: 'rgba(0,0,0)',
+    background: '#1e2e3e',
+    logo: '#adefd1ff',
+    btnBg: 'rgba(0,0,0,0.6)',
+    btnHover: '#603f83a1',
+    btnColor: '#F0F4F8',
+    
+}
