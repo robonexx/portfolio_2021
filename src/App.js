3b504b62029834453fd341514b859290a0f5fb0c
robertwagar <robertwagar@gmail.com>
2021-02-09 14:48:09 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import './App.css';
+import React from "react";
+import {ThemeProvider} from "styled-components";
+import  {useDarkMode} from './components/Toggle/UseDarkMode'
+import { GlobalStyles } from './components/Themes/GlobalStyle'
+import { lightTheme, darkTheme } from "./components/Themes/Themes"
+import ThemeToggle from "./components/Toggle/Toggler"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring'
+import Header from './components/Header';
+import Home from './components/Pages/Home/Home'
+import About from './components/Pages/About/About';
+import Projects from './components/Pages/Projects/Projects'
+import Contact from './components/Pages/Contact/Contact'
 
-function App() {
+const App= () => {
+
+  
+
+  const fade = useSpring({from: {opacity: 0}, opacity: 1});
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+
+  if(!mountedComponent) return <div/>
+
   return (
-    <div className="App">
+    <Router>
+       <animated.div className="App" style={fade}>
+        <ThemeProvider theme={themeMode}>
+          <ThemeToggle theme={theme} toggleTheme={themeToggler} />
+            <GlobalStyles/>
+              <Header />
+                <div className="content">
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/projects" component={Projects} />
+                    <Route exact path="/contact" component={Contact} />
+                  </Switch>
+                  </div>
+            </ThemeProvider> 
+         
       
-    </div>
+      </animated.div>
+      </Router>
   );
-}
+};
 
 export default App;
