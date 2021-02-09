41299b1441e290e6c860a4a3bf0c2a45755f7a24
robertwagar <robertwagar@gmail.com>
2021-02-09 15:09:07 +0100

    extras, clock, weather and typewriter effect

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import WeatherCard from '../Weather/WeatherCard';
+
+
+// Easy version of setting a weather forecast. only shows stockholm now, could use geolocation to get the city or town of the user
+
+const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=stockholm,se&APPID=`
+const API_KEY = "037faff6758526984b82d7fb60ff4947" 
+const END = "&units=metric"
+
+
+function Weather() {
+
+    const [data, setData] = useState([]);
+
+ 
+    useEffect(() => {
+        fetch(API_URL + API_KEY + END)
+            .then(response => response.json())
+            .then(data => setData({ data })
+            );
+
+    }, []);
+
+    
+    return (
+        <div>
+                    <WeatherCard key={data.id} data={data}/>
+        </div>
+    )
+}
+
+export default Weather;
