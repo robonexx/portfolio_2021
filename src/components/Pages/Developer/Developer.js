5df3a4f86897d2905df98000545244226fb05b12
robonexx <robertwagar@gmail.com>
2021-10-05 13:02:10 +0200

    changed text

--- src/components/Pages/Developer/Developer.js
+++ src/components/Pages/Developer/Developer.js
@@ -14,7 +14,7 @@ const Developer = () => {
                                     <p>Im a developer based in Stockholm, Sweden. </p><br />
                                     <p>currently I'm going to the Front-end developer education at KYH in Stockholm.
                                             I also study daily on Udemy and other online sources to become better and continue to learn all things development. </p><br />
-                                    <p>I will shortly start my internship at Addoce in Stockholm and continue my process in developemnt there. My internship ends in april 2022 and my education is finnished in end of may 2022, and from there on hopefully get to work as a developer</p><br/>
+                                    <p>I will shortly start my internship at Addcode in Stockholm and continue my process in developemnt there. My internship ends in april 2022 and my education is finnished in end of may 2022, and from there on hopefully get to work as a developer</p><br/>
                                     <p>My main experience is in front-end development but I also have a passion for design. Given that producing a modern website requires the combination of design, server technology, and the layer that users interacts with, I believe having experience in both design and development allows for making the most optimal user experiences.</p><br />
                                     <p>My goal is to make a better web, <br />
                                             - one that is fast, easy to use, beautiful, accessible to all, and frustration-free. Regardless of your specific business requirements, in solving these challenges, you have a great chance of finding success online.</p>
